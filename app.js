3d99d884ff193604435dd56bc70dabc3bb925834
loretari <loretari@gmail.com>
2022-11-16 12:59:09 +0200

    init: initial firs commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+    links.classList.toggle("show-links");
+    console.log(links.classList.contains("show-links"));
+})
